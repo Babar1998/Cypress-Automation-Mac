6bd53fb7ba41a856e693e7ff04e0660fe27e76c2
Babar Arif <babararifmasood98@gmail.com>
2024-02-27 21:00:52 +0500

    learning

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,9 @@
 
 describe('Login Test', () => {
 
-    // it('ALS Agent Login', () => {
-    //     cy.visit("https://beta.alsharqi.co/core-ui/#/login")
-    // })
+    it('ALS Agent Login', () => {
+        cy.visit("https://beta.alsharqi.co/core-ui/#/login")
+    })
 
     it('Erohal Login', () => {
         cy.visit("https://acc.erohal.io/#/login")
