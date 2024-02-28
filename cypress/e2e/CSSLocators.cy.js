68f033f43eabb71204cf3eeb0ae6124dc331691b
Babar Arif <babararifmasood98@gmail.com>
2024-02-28 22:24:55 +0500

    Learning

--- cypress/e2e/CSSLocators.cy.js
+++ cypress/e2e/CSSLocators.cy.js
@@ -2,13 +2,18 @@
 describe('CSSLocators', () => {
 
     it('csslocators', () => {
-        cy.visit("http://localhost:5173/user/login")
+        cy.visit("http://localhost:5173/")
+        cy.wait(5000)
+        cy.get(".nav-item").contains("Login").click()
         cy.get("[name='email']").type("babar@gmail.com")
         cy.get("[name='password']").type("123456")
+        cy.wait(5000)
         cy.get("[type='submit']").click()
+        cy.wait(5000)
         cy.get(".nav-item").contains("Products").click()
+        cy.wait(5000)
         cy.get("[type='search']").type("Acer")
-        cy.wait(10000)
+        cy.wait(5000)
         cy.get(".nav-item").contains("Logout").click()
         
     })
