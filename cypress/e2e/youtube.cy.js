ef004983c3cd09505cc90fb1a42a3a6f9bde3eb7
Babar Arif <babararifmasood98@gmail.com>
2024-02-28 20:52:14 +0500

    learning

---
+++ cypress/e2e/youtube.cy.js
@@ -0,0 +1,18 @@
+
+
+describe('Daraz', () => {
+
+    it('daraz', () => {
+        cy.visit("https://www.youtube.com/")
+        // cy.get("[name='email']").type("babar@gmail.com")
+        // cy.get("[name='password']").type("123456")
+        // cy.get("[type='submit']").click()
+        // cy.wait(2000)
+        // cy.get(".nav-item").contains("Products").click()
+        // cy.get("[type='search']").type("Acer")
+        // cy.wait(5000)
+        // cy.get(".nav-item").contains("Logout").click()
+        
+    })
+
+  })
