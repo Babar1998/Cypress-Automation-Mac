c3bbf0ca904275b7de4b2bddd33189a0a5963a8c
Babar Arif <babararifmasood98@gmail.com>
2024-03-03 17:00:43 +0500

    chained xpaths

--- cypress/e2e/XPathLocators.cy.js
+++ cypress/e2e/XPathLocators.cy.js
@@ -8,4 +8,11 @@ describe('XPATHLocators', () => {
         
     })
 
+    it('chained xpaths', () => {
+        cy.visit("http://localhost:5173/products")
+        cy.wait(10000)
+        cy.xpath("//div[contains(@class,'bg-white h-100vh')]").xpath(".//a").should('have.length', 20)
+        
+    })
+
   })
