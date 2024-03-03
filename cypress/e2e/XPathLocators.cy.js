4766b3e5adad6c213d6f6c998647b906ec800eb7
Babar Arif <babararifmasood98@gmail.com>
2024-03-03 16:32:41 +0500

    xpath locators

---
+++ cypress/e2e/XPathLocators.cy.js
@@ -0,0 +1,11 @@
+
+describe('XPATHLocators', () => {
+
+    it('no. of products', () => {
+        cy.visit("http://localhost:5173/products")
+        cy.wait(10000)
+        cy.xpath("//div[contains(@class,'bg-white h-100vh')]//a").should('have.length', 20)
+        
+    })
+
+  })
